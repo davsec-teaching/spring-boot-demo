ed0491b7a7100fe19f4c0456d3f05afc68875cf4
T Palit <tpalit@cs.stonybrook.edu>
2025-02-13 21:22:48 -0800

    First commit

---
+++ src/main/java/com/ecs160/hw2/MyController.java
@@ -0,0 +1,36 @@
+package com.ecs160.hw2;
+
+import org.springframework.web.bind.annotation.*;
+
+class MyRequest {
+    private String postDate;
+    private String postContent;
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+}
+
+@RestController
+@RequestMapping("/myservice")
+public class MyController {
+    /*
+    curl.exe -X POST http://localhost:30000/moderate -H "Content-type: application/json" -d '{\"postContent\": \"Hello, duck Spring Boot!\"}'
+     */
+    @PostMapping("/sayhello")
+    public String sayHello(@RequestBody MyRequest request) {
+        return "";
+    }
+}
